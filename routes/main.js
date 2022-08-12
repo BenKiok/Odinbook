efb0412333a3b8e4f072a0d13407861f09061448
Benedict Kioko <benedictmkioko@gmail.com>
2022-08-12 11:34:38 -0400

    Add semicolon

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const express = require('express'),
       router = express.Router();
 
 router.get('/timeline', (req, res, next) => {
-  res.render('index', {title: 'Timeline'})
+  res.render('index', {title: 'Timeline'});
 });
 
 module.exports = router;
