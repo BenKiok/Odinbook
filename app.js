3f230689a71a17268d45df2e1820eb316e9b3c3d
Benedict Kioko <benedictmkioko@gmail.com>
2022-08-21 12:46:58 -0400

    Remove login user print to console

--- app.js
+++ app.js
@@ -35,7 +35,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  console.log(req.user);
   res.locals.user = req.user;
   next();
 });
